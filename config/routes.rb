ece3a8dddfe78e3ce78196fbbf92db2ef53fb826
jimenezmiguela <jimenezmiguela@yahoo.com>
2020-06-18 13:20:18 -0700

    Rails Validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
  Rails.application.routes.draw do
+  resources :customers
   resources :posts
     resources :comments
   # get /pages path
---
+++ spec/views/customers/index.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "customers/index", type: :view do
+  before(:each) do
+    assign(:customers, [
+      Customer.create!(
+        first_name: "First Name",
+        last_name: "Last Name",
+        phone: "Phone",
+        email: "Email"
+      ),
+      Customer.create!(
+        first_name: "First Name",
+        last_name: "Last Name",
+        phone: "Phone",
+        email: "Email"
+      )
+    ])
+  end
+
+  it "renders a list of customers" do
+    render
+    assert_select "tr>td", text: "First Name".to_s, count: 2
+    assert_select "tr>td", text: "Last Name".to_s, count: 2
+    assert_select "tr>td", text: "Phone".to_s, count: 2
+    assert_select "tr>td", text: "Email".to_s, count: 2
+  end
+end
